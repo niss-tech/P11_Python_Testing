6579d1d633c06a1f8ab09d705190588ec50ecf89
nisrine adamo <nisrineadamo1@gmail.com>
2025-06-28 22:38:26 +0200

    fix message quand email invalide

--- server.py
+++ server.py
@@ -24,10 +24,18 @@ clubs = loadClubs()
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    email_entered = request.form['email']
+    matching_clubs = [club for club in clubs if club['email'] == email_entered]
+
+    if not matching_clubs:
+        flash("Sorry, that email wasn't found.")
+        return redirect(url_for('index'))
+
+    club = matching_clubs[0]
+    return render_template('welcome.html', club=club, competitions=competitions)
+
 
 
 @app.route('/book/<competition>/<club>')
